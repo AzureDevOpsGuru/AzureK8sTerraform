6bc922e6ee7de7a825d6ee6a6730c69e5dd6fdf5
Khaki <ameni.mil.af@outlook.com>
2022-01-19 13:23:57 -0700

    Second commit '

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v1-terraform-azurerm-provider/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v1-terraform-azurerm-provider/main.tf
@@ -1,6 +1,6 @@
 # Configure Azure Provider
 provider "azurerm" {
-  # Version is optional, 
+  # Version is optional
   # Terraform recommends to pin to a specific version of provider
   #version = "=2.35.0"
   #version = "~>2.35.0"
